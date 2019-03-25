c0a0d5d1870ec576d9d495051b8660c78a0ebc49
Niyodusenga Jean Bosco <bosco7209@gmail.com>
2019-03-25 20:12:50 +0200

    [Feature #164832544] Add build script

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+app.get('/', (req, res) => {
+  res.send('Good');
+})
 
 app.listen(PORT, () => console.log(`App starts at PORT=${PORT}`));
