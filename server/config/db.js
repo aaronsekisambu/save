f08af3e5cf4592eb1df1be9e53be087b534988f7
avpaul <vabimana@gmail.com>
2019-03-26 13:32:50 +0200

    [chore #164866041] Setup database and eslint

---
+++ server/config/db.js
@@ -0,0 +1,24 @@
+import {Pool} from 'pg';
+import ENV from 'dotenv';
+
+ENV.config();
+
+class Database{
+constructor(){
+    this.connectionString = process.env.DATABASE_URL;
+    this.pool = new Pool({
+        connectionString: this.connectionString,
+    });
+    this.pool.on('connect' ,() =>
+        console.log('connected to the database on' + this.connectionString)
+    );
+    this.pool.on('eror', (error,client) =>
+        console.error(`unexpected error on idle client ${client} ${error}` )
+    );
+    this.pool.on('remove', () => console.log('client removed')
+    )
+}
+
+}
+
+export default new Database();
