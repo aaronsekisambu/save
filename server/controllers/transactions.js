0b744604f18022af614b12c3f9fb6d677dd7f78e
aaronsekisambu <aaron.sekisambu@gmail.com>
2019-03-27 14:12:12 +0200

    Adding saveMoney model

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -1,11 +1,12 @@
 // import will go here
+import transcat  from '../models/transactions';
 
 // the class for the transaction
 
 const transaction = {
     // methodes will go here and you need to follow the same method
-     async save () {
-        console.log('the saving')
+     async save (req, res) {
+         const saver = await transcat.saveMoney()
     }
 }
 
--- server/models/transactions.js
+++ server/models/transactions.js
@@ -1,10 +1,27 @@
 // imports goes here
+import db from '../config/db';
 class Transaction {
 
-  
-    async Transaction(data) {
-        console.log('Transaction');
+    async saveMoney(money) {
+        this.savedMoney = [
+            money.transactionId,
+            money.amount,
+            money.userId,
+            money.transactionCode,
+            money.comment
+        ];
+    try {
+        const saveMoney = await db.pool(`INSERT INTO 
+        transactions(transactionId, amount, userId, transactionDate, transactionCode, comment)
+        VALUES ($1, $2, $3, $4, $5, $6) returning *`,
+        this.savedMoney);
+        return saveMoney.rows[0];
     }
-  }
-  
+    catch (error) {
+        console.log(error);
+        return false
+    }
+    }
+};
+
   export default new Transaction();
