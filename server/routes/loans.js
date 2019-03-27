51a9f95626a75c0ac1df00e5e5a6c25445ce7ffd
Niyodusenga Jean Bosco <bosco7209@gmail.com>
2019-03-27 17:57:57 +0200

    [Feature #164832544] Fix abortRetry joy error

--- server/routes/loans.js
+++ server/routes/loans.js
@@ -4,7 +4,8 @@ import controller from '../controllers/loans.js';
 import validation from '../helpers/loansValidations';
 
 const route = express.Router();
+const isValid = (params) => celebrate(params, { abortEarly: false });
 
-route.post('/api/v1/loan', celebrate(validation.payLoan), controller.payLoan);
+route.post('/api/v1/loan', isValid(validation.payLoan), controller.payLoan);
 
 export default route;
