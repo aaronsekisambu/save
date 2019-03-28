3cbad9ca0bd629a1e4ab514ac38cb5c2603a4ad7
shemaeric <shemaeric01@gmail.com>
2019-03-28 10:38:43 +0200

    refactor the model

--- server/controllers/loans.js
+++ server/controllers/loans.js
@@ -6,14 +6,22 @@ import Loan from '../models/loans';
 const loan = {
     // methodes will go here and you need to follow the same method
      async requestLoan (req, res) {
-				const newLoan = Loan.requestLoan(req.body);
-				newLoan.then(loan => res.status(200).send({
-					status: 200,
-					message: 'the loan request has already been sent',
-					data: loan,
-				}))
-				.catch(err => console.log(err));
-    }
+				const newLoan = await Loan.requestLoan(req.body)
+				.then((loan) => {
+					if(!loan) {
+						return res.status(400).send({
+							status: 400,
+							errorMessage: 'you have to provide all datas',
+						})
+					}
+					res.status(200).send({
+						status: 200,
+						message: 'the loan request has already been sent',
+						data: loan,
+					})			
+			})
+			.catch(err => console.log(err));
+				}
 }
 
 export default loan;
