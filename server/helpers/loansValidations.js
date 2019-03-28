8edd73f8265c5b2d74649110d543a3e4a99983f2
Niyodusenga Jean Bosco <bosco7209@gmail.com>
2019-03-28 08:36:21 +0200

    [Feature #164832544] Edit validation schema

--- server/helpers/loansValidations.js
+++ server/helpers/loansValidations.js
@@ -3,8 +3,8 @@ import Validate from 'celebrate';
 const payLoan = {
   body: {
     userId: Validate.Joi.string().required().trim(),
-    amount: Validate.Joi.string().required().trim(),
-    transactionDate: Validate.Joi.string().required().trim(),
+    amount: Validate.Joi.number().required(),
+    transactionDate: Validate.Joi.date().required(),
     transactionCode: Validate.Joi.string().required().trim(),
     comment: Validate.Joi.string().required().trim(),
   },
