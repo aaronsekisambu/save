d7e5f50f5ccce2828ec55afa4ef7a4db8c90a5a0
fridolinho <fridolinho@gmail.com>
2019-03-29 17:25:08 +0200

    [ft-164833457] fix indentation

--- server/config/queries.js
+++ server/config/queries.js
@@ -1,156 +1,158 @@
 const createUser = `
-    INSERT INTO users (
-      email,
-      salt,
-      hash
-    ) VALUES ($1,$2,$3)
-    RETURNING userId,email;
+	INSERT INTO users (
+	  email,
+	  salt,
+	  hash
+	) VALUES ($1,$2,$3)
+	RETURNING *;
   `;
 
 const updateUser = `
-        UPDATE users SET 
-                firstName = $1,
-                lastName = $2,
-                employmentDate = $3,
-                membershipDate = $4,
-                nationality = $5,
-                phoneNumber = $6,
-                email = $7,
-                profileImage = $8,
-                slackHandle = $9,
-                salt = $10,
-                hash = $11
-           WHERE userId = $12
-           RETURNING *; 
-    `;
+		UPDATE users SET 
+				firstName = $1,
+				lastName = $2,
+				employmentDate = $3,
+				membershipDate = $4,
+				nationality = $5,
+				phoneNumber = $6,
+				email = $7,
+				profileImage = $8,
+				slackHandle = $9,
+				salt = $10,
+				hash = $11
+		   WHERE userId = $12
+		   RETURNING *; 
+	`;
 const approveUser = `
-    UPDATE users 
-    SET membershipDate = now()
-    WHERE userId = $1
-    RETURNING *;
-    `
+	UPDATE users 
+	SET membershipDate = now()
+	WHERE userId = $1
+	RETURNING *;
+	`
 
 const getUser = `
         SELECT * FROM users
         where userId = $1;
     `;
+
 const getUserByEmail = `
         SELECT * FROM users
         where email = $1;
     `;
 
 const deleteUser = `
-        DELETE FROM users
-        WHERE userId = $1;
-    `;
+		DELETE FROM users
+		WHERE userId = $1;
+	`;
+	
 const createLoan = `
-        INSERT INTO loans (
-          "userid",
-          "guarantor",
-          "amount",
-          "interest",
-          "totalamount",
-          "paymentperiod",
-          "loanstatus",
-          "startdate"
-             ) VALUES (
-                    (SELECT userId from users WHERE userId = $1),
-                    (SELECT userId from users WHERE userId = $2),
-                    $3,
-                    $4,
-                    $5,
-                    $6,
-                    $7,
-                    $8
-                    )
-            RETURNING *;
-    `;
+		INSERT INTO loans (
+		  "userid",
+		  "guarantor",
+		  "amount",
+		  "interest",
+		  "totalamount",
+		  "paymentperiod",
+		  "loanstatus",
+		  "startdate"
+			 ) VALUES (
+					(SELECT userId from users WHERE userId = $1),
+					(SELECT userId from users WHERE userId = $2),
+					$3,
+					$4,
+					$5,
+					$6,
+					$7,
+					$8
+					)
+			RETURNING *;
+	`;
 
 const getUserLoans = `
-        SELECT * FROM loans 
-        WHERE userId = (SELECT userId from users WHERE userId = $1),
-        ORDER BY startDate DESC;
-    `;
+		SELECT * FROM loans 
+		WHERE userId = (SELECT userId from users WHERE userId = $1),
+		ORDER BY startDate DESC;
+	`;
 
 const getSingleUserLoan = `
-        SELECT * FROM loans
-        WHERE loanId = $1 AND userId = (SELECT userId from users WHERE userId = $2);
-    `;
+		SELECT * FROM loans
+		WHERE loanId = $1 AND userId = (SELECT userId from users WHERE userId = $2);
+	`;
 
 const getUserSavings = `
-        SELECT * FROM transactions
-        WHERE userId = (SELECT userId from users WHERE userId = $1)
-            AND transactionCode = $2
-        ORDER BY transactionDate DESC;
-    `;
+		SELECT * FROM transactions
+		WHERE userId = (SELECT userId from users WHERE userId = $1)
+			AND transactionCode = $2
+		ORDER BY transactionDate DESC;
+	`;
 const createTransaction = `
-        INSERT INTO transactions (
-                userId,
-                amount,
-                transactionDate,
-                transactionCode,
-                comment
-            ) VALUES (
-                (SELECT userId from users WHERE userId = $1),
-                $1,
-                $2,
-                $3,
-                $4,
-                $5) 
-            RETURNING *; 
-    `;
+		INSERT INTO transactions (
+				userId,
+				amount,
+				transactionDate,
+				transactionCode,
+				comment
+			) VALUES (
+				(SELECT userId from users WHERE userId = $1),
+				$1,
+				$2,
+				$3,
+				$4,
+				$5) 
+			RETURNING *; 
+	`;
 const getSingleTransaction = `
-        SELECT * FROM transactions
-        WHERE transactionId = $1;
-    `;
+		SELECT * FROM transactions
+		WHERE transactionId = $1;
+	`;
 
 const createUserTable = `
-        CREATE TABLE IF NOT EXISTS users(
-                userId UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                firstName VARCHAR(255),
-                lastName VARCHAR(255),
-                employmentDate DATE,
-                membershipDate DATE,
-                isAdmin BOOLEAN DEFAULT false,
-                nationality VARCHAR(50),
-                phoneNumber INTEGER,
-                email VARCHAR(255) UNIQUE,
-                profileImage VARCHAR(255),
-                slackHandle VARCHAR(50),
-                salt TEXT,
-                hash TEXT
-            );
-        `;
+		CREATE TABLE IF NOT EXISTS users(
+				userId UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+				firstName VARCHAR(255),
+				lastName VARCHAR(255),
+				employmentDate DATE,
+				membershipDate DATE,
+				isAdmin BOOLEAN DEFAULT false,
+				nationality VARCHAR(50),
+				phoneNumber INTEGER,
+				email VARCHAR(255) UNIQUE,
+				profileImage VARCHAR(255),
+				slackHandle VARCHAR(50),
+				salt TEXT,
+				hash TEXT
+			);
+		`;
 
 const createLoansTable = `
-    CREATE TABLE IF NOT EXISTS loans(
-        loanId UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-        userId UUID REFERENCES users(userId),
-        guarantor UUID REFERENCES users(userId),
-        amount INTEGER NOT NULL,
-        interest INTEGER NOT NULL,
-        totalAmount INTEGER NOT NULL,
-        paymentPeriod INTEGER NOT NULL,
-        loanStatus TEXT,
-        startDate DATE
-            );
-        `;
+	CREATE TABLE IF NOT EXISTS loans(
+		loanId UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+		userId UUID REFERENCES users(userId),
+		guarantor UUID REFERENCES users(userId),
+		amount INTEGER NOT NULL,
+		interest INTEGER NOT NULL,
+		totalAmount INTEGER NOT NULL,
+		paymentPeriod INTEGER NOT NULL,
+		loanStatus TEXT,
+		startDate DATE
+			);
+		`;
 
 const createTransactionsTable = `
-            CREATE TABLE IF NOT EXISTS transactions(
-                transactionId UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                amount INTEGER NOT NULL,
-                userId UUID REFERENCES users(userId),
-                transactionDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                transactionCode INTEGER NOT NULL,
-                comment  TEXT
-            );
-        `;
+			CREATE TABLE IF NOT EXISTS transactions(
+				transactionId UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+				amount INTEGER NOT NULL,
+				userId UUID REFERENCES users(userId),
+				transactionDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+				transactionCode INTEGER NOT NULL,
+				comment  TEXT
+			);
+		`;
 
 const dropTables = `
-           DROP TABLE IF EXISTS 
-           users,loans,transactions;
-    `;
+		   DROP TABLE IF EXISTS 
+		   users,loans,transactions;
+	`;
 
 export default {
   createUser,
