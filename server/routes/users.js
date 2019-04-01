054ef11d2db5ddad0a61fdb6ad0e34c0796f70e1
Niyodusenga Jean Bosco <bosco7209@gmail.com>
2019-04-01 15:15:16 +0200

    [Chore #165021309] Check admin token on delete user

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { celebrate } from 'celebrate';
 import userController from '../controllers/users';
 import userValidation from '../helpers/userValidations';
+import token from '../middleware/Auth';
 
 const router = express.Router();
 const isValid = params => celebrate(params, { abortEarly: false });
+const verifyToken = token.verifyToken();
 
 router.post('/api/v1/auth/signup',
   isValid(userValidation.userSignup),
@@ -12,6 +14,7 @@ router.post('/api/v1/auth/signup',
 
 router.delete('/api/v1/users/:id',
   isValid(userValidation.deleteUser),
+  verifyToken,
   userController.deleteUser);
 
 router.post('/api/v1/auth/login',
