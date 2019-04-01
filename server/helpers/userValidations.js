b0fe6afce7d8f1e1bb9181233fe596d231e5f901
fridolinho <fridolinho@gmail.com>
2019-04-01 14:42:13 +0200

    [ft-164833612] Add userId validation

--- server/helpers/userValidations.js
+++ server/helpers/userValidations.js
@@ -8,20 +8,20 @@ const checkUserId = {
 
 const userSignup = {
   body: {
-    email: Validate.Joi.string().required(),
+    email: Validate.Joi.string().email().required(),
     password: Validate.Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
   },
 };
 
 const userLogin = {
   body: {
-    email: Validate.Joi.string().required(),
+    email: Validate.Joi.string().email().required(),
     password: Validate.Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
   },
 };
 
 export default {
-  deleteUser,
+  dcheckUserId,
   userSignup,
   userLogin,
 };
